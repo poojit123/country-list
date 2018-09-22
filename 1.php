f8b582e03c75be4cb3fb87507c96ce5bb6bf779c
poojit143 <jmotar.29@gmail.com>
2018-09-22 08:16:57 +0530

    first commit

---
+++ 1.php
@@ -0,0 +1,23 @@
+<?php 
+ $file='country-list.json';
+ $content=file_get_contents($file);
+ $object= json_decode( $content);
+ $countryCode='BD'; // here is selected country code.
+
+ $array=(array)$object; // convert object to array.
+
+ function searchForId($id, $array) {
+   foreach ($array as $key => $val) {
+       if ($key === $id) {
+           return $val;
+       }
+   }
+   return null;
+ }
+ 
+ // show country name from selected country code. 
+ foreach ($array as $key => $value) {
+ 	$newArray= (array)$value;
+ 	 echo searchForId($countryCode, $newArray);
+ }
+ ?>
